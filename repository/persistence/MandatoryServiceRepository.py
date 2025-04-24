c78404a70bac44f8408b9ca31dee46c12ed7d556
Roger-2007 <rogerarenas81@outlook.es>
2025-04-24 01:21:34 -0500

    Se agregaron algunos metodos a optionalService y se hicieron ajustes en otras clases

--- repository/persistence/MandatoryServiceRepository.py
+++ repository/persistence/MandatoryServiceRepository.py
@@ -5,21 +5,21 @@ class MandatoryServiceRepository:
         self.mandatory_service = MandatoryService
 
     def create_mandatory_service_repository(self , mandatory_service , db):
-        query = "INSERT INTO mandatoryService (name,description,price) VALUES (%s,%s,%s)"
+        query = "INSERT INTO mandatoryservice (name,description,price) VALUES (%s,%s,%s)"
         values = mandatory_service.name , mandatory_service.description , mandatory_service.price
         db.execute_query(query,values)
 
     def all_mandatory_service_repository(self,db):
-        query = "SELECT * FROM mandatoryService"
+        query = "SELECT * FROM mandatoryservice"
         result = db.execute_query(query)
         return result
 
     def update_mandatory_service_repository(self,mandatory_service,db):
-        query = "UPDATE mandatoryService set name=%s,description=%s,price=%s where id=%s"
+        query = "UPDATE mandatoryservice set name=%s,description=%s,price=%s where id=%s"
         values = (mandatory_service.name,mandatory_service.description,mandatory_service.price,mandatory_service.id)
         db.execute_query(query,values)
 
     def delete_mandatory_service_repository(self,mandatory_service,db):
-        query = "DELETE from mandatoryService where id=%s"
+        query = "DELETE from mandatoryservice where id=%s"
         values = (mandatory_service.id,)
         db.execute_query(query,values)
